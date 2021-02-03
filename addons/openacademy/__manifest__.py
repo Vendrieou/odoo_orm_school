a102243a2c7297483ce156c069c5dbe6f2a387a5
vendrie yulman <vendriey@gmail.com>
2021-02-03 15:41:53 +0700

    commit changes openacademy

--- addons/openacademy/__manifest__.py
+++ addons/openacademy/__manifest__.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "openacademy",
+    'name': "Open Academy",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': """Manage trainings""",
 
     'description': """
-        Long description of module's purpose
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
     """,
 
     'author': "My Company",
@@ -16,20 +16,27 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Test',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        # 'templates.xml',
+        'views/openacademy.xml',
+        'views/partner.xml',
+        'views/session_board.xml',
+        'reports.xml',
+  ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+        'demo.xml',
     ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
