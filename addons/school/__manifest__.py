29064c90704cad3b37b8468c8411d3f277e23e85
vendrie yulman <vendriey@gmail.com>
2021-02-03 14:35:13 +0700

    commit school module

--- addons/school/__manifest__.py
+++ addons/school/__manifest__.py
@@ -10,7 +10,7 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "Vendrie",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
@@ -20,16 +20,16 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','mail'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
--- addons/school/models/school.py
+++ addons/school/models/school.py
@@ -1,20 +1,9 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class school(models.Model):
     _name = 'school.profile'
-    _description = 'school.school'
 
     name = fields.Char(string="School Name")
     email = fields.Char(string="Email")
-    phone = fields.Char(string="Phone")
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
-
-    @api.depends('value')
-    def _value_pc(self):
-        for record in self:
-            record.value2 = float(record.value) / 100
+    phone = fields.Char("Phone")
