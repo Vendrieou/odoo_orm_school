99c7d44b890bb29f30e3ea1e46be6ddd1d2b8885
vendrie yulman <vendriey@gmail.com>
2021-02-04 17:27:46 +0700

    commit school addons list.view

--- addons/school/models/school.py
+++ addons/school/models/school.py
@@ -3,6 +3,7 @@ from odoo import models, fields
 
 class school(models.Model):
     _name = 'school.profile'
+    _description = 'school.description'
 
     name = fields.Char(string="School Name")
     email = fields.Char(string="Email")
